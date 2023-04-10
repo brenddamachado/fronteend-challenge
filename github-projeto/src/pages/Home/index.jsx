fa1e9d08e84637ba1e34df07212e27578f7ab46b
Brenda Machado <brenda16machado.bm@gmail.com>
2023-04-10 16:30:48 -0300

    atualizando a home e api 2

--- github-projeto/src/pages/Home/index.jsx
+++ github-projeto/src/pages/Home/index.jsx
@@ -1,36 +1,42 @@
 import styles from "./home.css";
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 export function Home() {
   // barra de pesquisa, user
-   const [user, setUser] = useState('') 
+  const [user, setUser] = useState("");
+  // lista das informaçoes do user
+  const [infoUser,setinfoUser] = useState('')
   //  button, pesquisar o user solicitado
-  const pesquisa=()=> async () =>{
-await axios
-// puxando a api
-.get(`https://api.github.com/users/${user}`)
-// resposta da pesquisa
-.then((response)=>{
-  console.log(response);
-})
-.catch((e) => console.log(e));
+  const pesquisa = async () => {
+    await axios
+      // puxando a api
+      .get(`https://api.github.com/users/${user}`)
+      // resposta da pesquisa
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((e) => console.log(e));
   };
   return (
     <>
       <div className="tudo">
-        <div className={styles.title}>
-            <h1>Search Devs</h1>
+        <div className='estilo'>
+          <h1>Search Devs</h1>
         </div>
 
-        <div > 
-          <input className="user" type="text" placeholder="Type the username here..."  onChange={(e)=> setUser(e.target.value)}/>
+        <div>
+          <input
+            className="user"
+            type="text"
+            placeholder="Type the username here..."
+            onChange={(e) => setUser(e.target.value)}
+          />
         </div>
 
         <div className="pesquisar">
-            <button onClick={()=> pesquisa()}>Buscar</button>
+          <button onClick={() => pesquisa()}>Buscar</button>
         </div>
-        
       </div>
     </>
   );
