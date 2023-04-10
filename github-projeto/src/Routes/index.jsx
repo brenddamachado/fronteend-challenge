e9f7abf91e10f1084fa5c8b64eed44728be0bbb6
Brenda Machado <brenda16machado.bm@gmail.com>
2023-04-09 22:32:18 -0300

    Ajuatando a Home.jsx

--- github-projeto/src/Routes/index.jsx
+++ github-projeto/src/Routes/index.jsx
@@ -3,7 +3,7 @@ import {Route, Routes} from 'react-router-dom'
 export function Router(){
     return(
         <Routes>
-            <Route path='/' element={<Home}/>
+            <Route path='/' element={<Home/>}/>
         </Routes>
     )
 }
