3f9702e31b5a511efb73b0f771778fba1c2f422f
Brenda Machado <brenda16machado.bm@gmail.com>
2023-04-10 01:43:12 -0300

    home e css

--- github-projeto/src/pages/Home/index.jsx
+++ github-projeto/src/pages/Home/index.jsx
@@ -1,11 +1,22 @@
-import styles from './home.css'
+import styles from "./home.css";
 
-
-
-export function Home(){
-    return (
-        <div className={styles.title}> 
+export function Home() {
+  return (
+    <>
+      <div className="tudo">
+        <div className={styles.title}>
             <h1>Search Devs</h1>
         </div>
-    )
+
+        <div>
+          <input type="text" placeholder="Type the username here..." />
+        </div>
+        
+        <div>
+            <button>Buscar</button>
+        </div>
+        
+      </div>
+    </>
+  );
 }
